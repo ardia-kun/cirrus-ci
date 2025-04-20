847350bc174d459b391226af24d80ca2e205ca12
texascake <tiktodz@soulvibe-team.cloud>
2025-04-20 07:23:17 +0700

    ci: trigger boeild(sL

--- final.sh
+++ final.sh
@@ -6,6 +6,7 @@ export TZ=Asia/Jakarta
 export CCACHE_DIR=~/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
+export CCACHE_COMPRESS=1
 ccache -M 100G -F 0
 ccache -o compression=true
 ccache -z
