f8d120ab1d79b7fab090d3a1b06b5d18671c5262
texascake <tiktodz@soulvibe-team.cloud>
2025-04-24 01:18:14 +0700

    ci: test X00TD codename()

--- upload.sh
+++ upload.sh
@@ -1,5 +1,6 @@
-if [ -f ~/rom/out/target/product/X00TD/lineage-rev-*.zip ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="📤 Uploading Build $(cd ~/rom/out/target/product/X00TD/ && ls lineage-rev-*.zip)"
-      rclone copy ~/rom/out/target/product/X00TD/lineage-rev-*.zip Gdrive:lineage -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="✅ Build $(cd ~/rom/out/target/product/X00TD/ && ls lineage-rev-*.zip) Uploaded Successfully!"
+if [ -f ~/rom/out/target/product/X00TD/lineage-*.zip ]; then
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="📤 Uploading Build $(cd ~/rom/out/target/product/X00TD/ && ls lineage-*.zip)"
+      rclone copy ~/rom/out/target/product/X00TD/lineage-*.zip Gdrive:lineage -P
+      rclone copy ~/rom/out/build_date.txt Gdrive:lineage -P
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="✅ Build $(cd ~/rom/out/target/product/X00TD/ && ls lineage-*.zip) Uploaded Successfully!"
 fi
