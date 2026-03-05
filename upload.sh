8691f7a802b1f6790dd0773a9b954b6dc489bc85
Ardia-kun <85530328+ardia-kun@users.noreply.github.com>
2026-03-05 22:45:27 +0700

    Update upload.sh [skip ci]

--- upload.sh
+++ upload.sh
@@ -3,5 +3,5 @@ if [ -f ~/rom/out/target/product/surya/lineage-*.zip ]; then
       rclone copy ~/rom/out/target/product/surya/lineage-*.zip kidz:lineage -P
       rclone copy ~/rom/out/target/product/surya/boot.img kidz:lineage -P
       rclone copy ~/rom/out/build_date.txt kidz:lineage -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="✅ Build $(cd ~/rom/out/target/product/X00TD/ && ls lineage-*.zip) Uploaded Successfully!"
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="✅ Build $(cd ~/rom/out/target/product/surya/ && ls lineage-*.zip) Uploaded Successfully!"
 fi
